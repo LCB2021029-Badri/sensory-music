12bc780be9ce23c98f23e477f5b9fde6b3b12594
Badri Akkala <lcb2021029@iiitl.ac.in>
2022-11-01 02:58:46 +0530

    step 2: creating data model class "AudioModel" to contain attributes of the cursor/musicFile

---
+++ app/src/main/java/com/example/sensorymusic/AudioModel.kt
@@ -0,0 +1,7 @@
+package com.example.sensorymusic
+
+import java.io.Serializable
+
+//this is a data model class
+//setter and getter
+class AudioModel (var path: String, var title: String, var duration: String) : Serializable
