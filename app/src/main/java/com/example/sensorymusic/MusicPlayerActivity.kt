39e932ec1c785ea7eb05b9b422aa59eb715fb6e5
Badri Akkala <lcb2021029@iiitl.ac.in>
2022-11-02 00:23:44 +0530

    step last: 100% working kotlin code without UI

--- app/src/main/java/com/example/sensorymusic/MusicPlayerActivity.kt
+++ app/src/main/java/com/example/sensorymusic/MusicPlayerActivity.kt
@@ -1,163 +1,162 @@
-package com.example.sensorymusic;
+package com.example.sensorymusic
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.example.sensorymusic.MyMediaPalyer.instance
+import com.example.sensorymusic.MyMediaPalyer.currentIndex
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import android.widget.SeekBar
+import com.example.sensorymusic.AudioModel
+import android.media.MediaPlayer
+import com.example.sensorymusic.MyMediaPalyer
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.ImageView
+import com.example.sensorymusic.R
+import com.example.sensorymusic.MusicPlayerActivity
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.Toast
+import java.io.IOException
+import java.util.ArrayList
+import java.util.concurrent.TimeUnit
 
-import android.media.MediaPlayer;
-import android.os.Bundle;
-import android.os.Handler;
-import android.provider.MediaStore;
-import android.widget.ImageView;
-import android.widget.SeekBar;
-import android.widget.TextView;
-import android.widget.Toast;
+class MusicPlayerActivity : AppCompatActivity() {
+    private lateinit var titleTv: TextView
+    private lateinit var currentTimeTv: TextView
+    private lateinit var totalTimeTv: TextView
+    private lateinit var seekBar: SeekBar
+    private lateinit var pausePlay: ImageView
+    private lateinit var nextBtn: ImageView
+    private lateinit var previousBtn: ImageView
+    private lateinit var musicIcon: ImageView
+    private lateinit var songsList: ArrayList<AudioModel>
+    private lateinit var currentSong: AudioModel
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
+    private var mediaPlayer = instance
+    var x = 0
 
-public class MusicPlayerActivity extends AppCompatActivity {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_player)
+        titleTv = findViewById(R.id.song_title)
+        currentTimeTv = findViewById(R.id.current_time)
+        totalTimeTv = findViewById(R.id.total_time)
+        seekBar = findViewById(R.id.seek_bar)
+        pausePlay = findViewById(R.id.pause_play)
+        nextBtn = findViewById(R.id.next)
+        previousBtn = findViewById(R.id.previous)
+        musicIcon = findViewById(R.id.music_icon_big)
 
-    TextView titleTv, currentTimeTv, totalTimeTv;
-    SeekBar seekBar;
-    ImageView pausePlay, nextBtn, previousBtn, musicIcon;
-    ArrayList<AudioModel> songsList;
-    AudioModel currentSong;
-    MediaPlayer mediaPlayer = MyMediaPalyer.INSTANCE.getInstance();
-    int x =0;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_music_player);
-
-        titleTv = findViewById(R.id.song_title);
-        currentTimeTv = findViewById(R.id.current_time);
-        totalTimeTv = findViewById(R.id.total_time);
-        seekBar = findViewById(R.id.seek_bar);
-        pausePlay = findViewById(R.id.pause_play);
-        nextBtn = findViewById(R.id.next);
-        previousBtn = findViewById(R.id.previous);
-        musicIcon = findViewById(R.id.music_icon_big);
-
-        titleTv.setSelected(true);
+        titleTv.setSelected(true)
 
         // here we get the song list passed from the adapter through the intent
-        songsList = (ArrayList<AudioModel>) getIntent().getSerializableExtra("LIST");
+        songsList = (intent.getSerializableExtra("LIST") as ArrayList<AudioModel>?)!!
 
         //to play the current song using the following method
-        setResourcesWithMusic();
+        setResourcesWithMusic()
 
         //observing changes in UI and reacting accordingly
-        MusicPlayerActivity.this.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
+        runOnUiThread(object : Runnable {
+            override fun run() {
                 //we check each second from UI to activate the seekbar
                 if (mediaPlayer != null) {
-                    seekBar.setProgress(mediaPlayer.getCurrentPosition());
-                    currentTimeTv.setText(convertToMMSS(mediaPlayer.getCurrentPosition() + ""));
-
-                    if (mediaPlayer.isPlaying()) {
-                        pausePlay.setImageResource(R.drawable.icon_pause);
-                        musicIcon.setRotation(x++);
+                    seekBar.setProgress(mediaPlayer!!.currentPosition)
+                    currentTimeTv.setText(convertToMMSS(mediaPlayer!!.currentPosition.toString() + ""))
+                    if (mediaPlayer!!.isPlaying) {
+                        pausePlay.setImageResource(R.drawable.icon_pause)
+                        musicIcon.setRotation(x++.toFloat())
                     } else {
-                        pausePlay.setImageResource(R.drawable.icon_play);
-                        musicIcon.setRotation(0);
+                        pausePlay.setImageResource(R.drawable.icon_play)
+                        musicIcon.setRotation(0f)
                     }
                 }
                 //smoothen the seekbar
-                new Handler().postDelayed(this, 100);
+                Handler().postDelayed(this, 100)
             }
-        });
+        })
 
         // if the seekbar is changes by the user
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
                 if (mediaPlayer != null && fromUser) {
-                    mediaPlayer.seekTo(progress);
+                    mediaPlayer!!.seekTo(progress)
                 }
             }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
     }
 
     //resources for playing the current song
-    void setResourcesWithMusic() {
-        currentSong = songsList.get(MyMediaPalyer.INSTANCE.getCurrentIndex());
-        titleTv.setText(currentSong.getTitle());
-        totalTimeTv.setText(convertToMMSS(currentSong.getDuration()));
-
-        pausePlay.setOnClickListener(v -> pausePlay());
-        nextBtn.setOnClickListener(v -> playNextSong());
-        previousBtn.setOnClickListener(v -> playPreviousSong());
+    fun setResourcesWithMusic() {
+        currentSong = songsList!![currentIndex]
+        titleTv!!.text = currentSong!!.title
+        totalTimeTv!!.text = convertToMMSS(currentSong!!.duration)
+        pausePlay!!.setOnClickListener { v: View? -> pausePlay() }
+        nextBtn!!.setOnClickListener { v: View? -> playNextSong() }
+        previousBtn!!.setOnClickListener { v: View? -> playPreviousSong() }
 
         //when the song is clicked form the recycler list
-        playMusic();
+        playMusic()
     }
-
 
     //methods to play song-----------
-    private void playMusic() {
+    private fun playMusic() {
         //we need a media player instance
-        mediaPlayer.reset();
+        mediaPlayer!!.reset()
         try {
-            mediaPlayer.setDataSource(currentSong.getPath());
-            mediaPlayer.prepare();
-            mediaPlayer.start();
-            seekBar.setProgress(0);
-            seekBar.setMax(mediaPlayer.getDuration());
-        } catch (IOException e) {
-            e.printStackTrace();
-            Toast.makeText(this, "there is an error", Toast.LENGTH_SHORT).show();
+            mediaPlayer!!.setDataSource(currentSong!!.path)
+            mediaPlayer!!.prepare()
+            mediaPlayer!!.start()
+            seekBar!!.progress = 0
+            seekBar!!.max = mediaPlayer!!.duration
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Toast.makeText(this, "there is an error", Toast.LENGTH_SHORT).show()
         }
     }
 
-    private void playNextSong() {
+    private fun playNextSong() {
         //if not last song
-        MyMediaPalyer.currentIndex += 1;
-        mediaPlayer.reset();
-        setResourcesWithMusic();
+        currentIndex += 1
+        mediaPlayer!!.reset()
+        setResourcesWithMusic()
         //if last song
-        if (MyMediaPalyer.currentIndex == songsList.size() - 1) {
-            return;
+        if (currentIndex == songsList!!.size - 1) {
+            return
         }
     }
 
-    private void playPreviousSong() {
+    private fun playPreviousSong() {
         //if not first song
-        MyMediaPalyer.currentIndex -= 1;
-        mediaPlayer.reset();
-        setResourcesWithMusic();
+        currentIndex -= 1
+        mediaPlayer!!.reset()
+        setResourcesWithMusic()
         //if first song
-        if (MyMediaPalyer.currentIndex == 0) {
-            return;
+        if (currentIndex == 0) {
+            return
         }
     }
 
-    private void pausePlay() {
-        if (mediaPlayer.isPlaying()) {
-            mediaPlayer.pause();
+    private fun pausePlay() {
+        if (mediaPlayer!!.isPlaying) {
+            mediaPlayer!!.pause()
         } else {
-            mediaPlayer.start();
+            mediaPlayer!!.start()
         }
     }
 
-    //for the millisecond format of the duration
-    public static String convertToMMSS(String duration) {
-        //first convert duration to milliseconds then to seconds and minutes
-        Long millis = Long.parseLong(duration);
-        return String.format("%02d:%02d",
+    companion object {
+        //for the millisecond format of the duration
+        fun convertToMMSS(duration: String): String {
+            //first convert duration to milliseconds then to seconds and minutes
+            val millis = duration.toLong()
+            return String.format(
+                "%02d:%02d",
                 TimeUnit.MILLISECONDS.toMinutes(millis) % TimeUnit.HOURS.toMinutes(1),
-                TimeUnit.MILLISECONDS.toSeconds(millis) % TimeUnit.MINUTES.toSeconds(1));
+                TimeUnit.MILLISECONDS.toSeconds(millis) % TimeUnit.MINUTES.toSeconds(1)
+            )
+        }
     }
-
 }
